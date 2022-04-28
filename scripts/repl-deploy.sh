7a91b6d6437fbb55015b2a1f1a07e5e5ee16d118
Akhil Sadam <sadam.akhil@gmail.com>
2022-04-28 13:14:26 -0500

    updating Kubernetes

--- scripts/repl-deploy.sh
+++ scripts/repl-deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ../
 cat ~/portinfo > pi 
-lines=$(echo $pi | tr ";" "\n")
-echo ${lines}
+lines=$(echo $pi | tr ":" "\n")
+echo $(lines)
 # firstString=$(<deployment-flask.yml)
 # firstStringTwo=$(<deployment-worker.yml)
 # secondString=$(echo $(kubectl get services compose-redis-service --output=jsonpath="{.spec.clusterIP}") | tr -d '"')
