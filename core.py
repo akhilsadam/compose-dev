7c9e86651e293148c35f9382ec6aa3bb4cba5afb
Akhil Sadam <sadam.akhil@gmail.com>
2022-04-29 10:49:52 -0500

    proxy-fixes

--- core.py
+++ core.py
@@ -12,11 +12,12 @@ import app.log as log
 logger = log.init_logger('root')
 
 # # setup reverse proxy if on kubernetes
+logger.info("Checking for Proxy...")
 try: proxy = sys.argv[2]
 except:
-    logger.info("Proxy not found...")
+    logger.info("Proxy not found.")
 else:
-    logger.info(f"Proxy {proxy}")
+    logger.info(f"Found Proxy : {proxy}")
     app.options.options.setproxy(proxy)
 
 from app import init_app
